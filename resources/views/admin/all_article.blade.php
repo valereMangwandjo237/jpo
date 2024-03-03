4f2e728a99985b4e72b6dc422f929a6bd1f85c43
sokcellerie <vmangwandjo@gmail.com>
2024-03-03 22:10:29 +0100

    add boostrap and jquery, create database

--- resources/views/admin/all_article.blade.php
+++ resources/views/admin/all_article.blade.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
     <title>Document</title>
 </head>
 <body>
